8cfcf66d14f8909ca2dadb6396ae1d7bae778237
Johan Alexander Farfan Sierra <johanfarfan25@gmail.com>
2025-03-27 14:29:06 -0500

    mejora

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.user_type.auth')
 
 @section('content')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <div class="col-xl-12 col-sm-6 mb-xl-0 mb-4">
         <div class="col-auto my-auto p-3">
